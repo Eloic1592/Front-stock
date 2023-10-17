9e9d111fba09d4319eb2ae0073b14dfdd04509b9
Eloic <99272320+Eloic1592@users.noreply.github.com>
2023-10-17 11:30:21 +0300

    Update View.sql

--- BDD/View.sql
+++ BDD/View.sql
@@ -5,4 +5,21 @@ select e.id,te.type_entretien,e.entretien,m.materiel,e.etat from entretien e
 join type_entretien te on te.id=e.idtype_entretien 
 join materiel m on m.id=e.idmateriel;
 
+-- View disponibilite
+Drop view v_dispo;
+Create or replace view v_dispo as 
+select d.*,t.nom,t.prenom from disponibilite d join technicien t on t.id=d.idtechnicien;
 
+
+
+-- A faire cette apres-midi
+select * from technicien;
+
+select * from suggestion;
+
+select * from historique;
+
+select * from type_entretien;
+
+-- Vue
+Tache,Intervention,tache_acheve,tache_prioritaires
