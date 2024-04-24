522b8fd3ddd030091c4a8a6eeed113e5a3c182df
Eloic Rakoto-Arisoa <99272320+Eloic1592@users.noreply.github.com>
2024-04-24 11:13:24 +0300

    Update Dashboard.jsx

--- src/app/views/admin/Dashboard/Dashboard.jsx
+++ src/app/views/admin/Dashboard/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { Box, TextField, Select, MenuItem, Grid } from '@mui/material';
+import { Box, TextField, Select, MenuItem, Grid, Button, Typography } from '@mui/material';
 import { Breadcrumb, SimpleCard } from 'app/components';
 import { months } from 'app/utils/utils';
 import { useEffect, useState } from 'react';
@@ -7,12 +7,13 @@ import { Container } from 'app/views/style/style';
 import FIFO from './FIFO';
 import LIFO from './LIFO';
 import Rotationstock from './Rotationstock';
+import Switch from '@mui/material/Switch';
 
 const Dashboard = () => {
   // Input
-  const [annee, setAnnee] = useState(0);
+  const [datemin, setDatemin] = useState('');
+  const [datemax, setDatemax] = useState('');
   const [month, setMonth] = useState('0');
-  const [method, setMethod] = useState('0');
   const [initialDataFetched, setInitialDataFetched] = useState(false);
   const [message, setMessage] = useState({
     text: 'Information enregistree',
@@ -25,6 +26,12 @@ const Dashboard = () => {
     fifos: [],
     lifos: []
   });
+  const [selectedValue, setSelectedValue] = useState('Unite');
+
+  const handleChange = () => {
+    setSelectedValue(selectedValue === 'Unite' ? 'Monetaire' : 'Unite');
+    // alert(selectedValue);
+  };
 
   //  Use effect
   useEffect(() => {
@@ -79,50 +86,80 @@ const Dashboard = () => {
       </Box>
       <Grid container direction="column" spacing={2}>
         <Grid item>
-          <SimpleCard title="" sx={{ marginBottom: '16px' }}>
-            <form>
-              <div style={{ display: 'flex', gap: '16px' }}>
-                <Grid container spacing={2}>
-                  <Grid item xs={12}>
-                    <Select
-                      fullWidth
-                      labelId="month-select-label"
-                      variant="outlined"
-                      size="small"
-                      value={month}
-                      onChange={(event) => setMonth(event.target.value)}
-                      sx={{ mb: 3 }}
-                    >
-                      <MenuItem value="0">Tous les mois</MenuItem>
-                      {months.map((monthName, index) => (
-                        <MenuItem key={index + 1} value={index + 1}>
-                          {monthName}
-                        </MenuItem>
-                      ))}
-                    </Select>
-                  </Grid>
-                  {/* <Grid item xs={6}>
-                    <Select
-                      fullWidth
-                      labelId="month-select-label"
-                      variant="outlined"
-                      size="small"
-                      value={method}
-                      onChange={(event) => setMethod(event.target.value)}
-                      sx={{ mb: 3 }}
-                    >
-                      <MenuItem value="0">Rotation de stock</MenuItem>
-                      <MenuItem key="1" value="1">
-                        FIFO
-                      </MenuItem>
-                      <MenuItem key="2" value="2">
-                        LIFO
-                      </MenuItem>
-                    </Select>
-                  </Grid> */}
-                </Grid>
-              </div>
-            </form>
+          <SimpleCard title="Rechercher" sx={{ marginBottom: '16px' }}>
+            <Grid container spacing={2}>
+              <Grid item xs={3}>
+                <TextField
+                  fullWidth
+                  size="small"
+                  type="date"
+                  name="annee"
+                  variant="outlined"
+                  value={datemin}
+                  onChange={(event) => setDatemin(event.target.value)}
+                  sx={{ mb: 3 }}
+                />
+              </Grid>
+              <Grid item xs={3}>
+                <TextField
+                  fullWidth
+                  size="small"
+                  type="date"
+                  name="annee"
+                  variant="outlined"
+                  value={datemax}
+                  onChange={(event) => setDatemax(event.target.value)}
+                  sx={{ mb: 3 }}
+                />
+              </Grid>
+              <Grid item xs={3}>
+                <Select
+                  fullWidth
+                  labelId="month-select-label"
+                  variant="outlined"
+                  size="small"
+                  value={month}
+                  onChange={(event) => setMonth(event.target.value)}
+                  sx={{ mb: 3 }}
+                >
+                  <MenuItem value="0">Tous les mois</MenuItem>
+                  {months.map((monthName, index) => (
+                    <MenuItem key={index + 1} value={index + 1}>
+                      {monthName}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </Grid>
+              <Grid item xs={3}>
+                <Button color="primary" variant="contained">
+                  Rechercher
+                </Button>
+              </Grid>
+            </Grid>
+          </SimpleCard>
+        </Grid>
+        <Grid item>
+          <SimpleCard title="Rotation de stock" sx={{ marginBottom: '16px' }}>
+            <Grid component="label" container alignItems="center" spacing={1}>
+              <Grid item>
+                <Typography>Unite</Typography>
+              </Grid>
+              <Grid item>
+                <Switch
+                  checked={selectedValue === 'Unite'}
+                  onChange={handleChange}
+                  color="default"
+                  inputProps={{
+                    'aria-label': 'switch between Unite and Monetaire',
+                    checked: 'Unite',
+                    unchecked: 'Monetaire'
+                  }}
+                />
+              </Grid>
+              <Grid item>
+                <Typography>Monetaire</Typography>
+              </Grid>
+            </Grid>
           </SimpleCard>
         </Grid>
         <Grid item>
